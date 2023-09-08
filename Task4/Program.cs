80329c34d347599133b019a1d634968926ca767f
g0dl3ss1337 <g0dl3ss1337@gmail.com>
2023-09-09 00:41:55 +0300

    added Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            User user = new User("g0dl3ss1337", "Максим", "Позіхайло", 18);
+            user.Print();
+        }
+    }
+
+}
---
+++ Task4/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task4
+{
+    class User
+    {
+        public string Login;
+        public string FirstName;
+        public string LastName;
+        public int Age;
+        public DateTime RegistrationDate;
+
+        public User(string login, string firstName, string lastName, int age)
+        {
+            Login = login;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            RegistrationDate = DateTime.Now;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Логін: {Login}\n" +
+                            $"Ім'я: {FirstName}\n" +
+                            $"Прізвище: {LastName}\n" +
+                            $"Вік: {Age}\n" +
+                            $"Дата заповнення анкети: {RegistrationDate}\n");
+        } 
+    }
+}
